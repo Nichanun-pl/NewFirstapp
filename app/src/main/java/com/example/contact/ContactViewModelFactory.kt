8d4a7d46a50ecb644847c8d1153d853ec249952c
Nichanun <nichanun.pl@rmuti.ac.th>
2020-08-25 21:05:42 +0700

    Update

--- app/src/main/java/com/example/contact/ContactViewModelFactory.kt
+++ app/src/main/java/com/example/contact/ContactViewModelFactory.kt
@@ -1,4 +1,20 @@
 package com.example.contact
 
-class ContactViewModelFactory {
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.data.ContactDAO
+
+class ContactViewModelFactory(
+    private val dataSource: ContactDAO,
+    private val application: Application
+) : ViewModelProvider.Factory {
+
+    @Suppress("Unchecked")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(ContactViewModel::class.java)){
+            return ContactViewModel(dataSource,application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel Class")
+    }
 }
